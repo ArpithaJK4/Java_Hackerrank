ed8e3e0495dc00063351e315096b5f653e34d47b
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-10-11 05:20:53 -0700

    Create JavaEndofif.java

---
+++ JavaEndofif.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      Scanner sc =new Scanner(System.in);
+      String s1= sc.nextLine();
+      String s2 =sc.nextLine();
+      String s3 =sc.nextLine();
+      System.out.println("1 "+s1);
+      System.out.println("2 "+s2);
+      System.out.println("3 "+s3);
+    }
+}
