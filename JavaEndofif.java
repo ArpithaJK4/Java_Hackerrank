615468007a28e93408256bf2c9efeb22e112e03d
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-10-11 05:25:57 -0700

    Update JavaEndofif.java

--- JavaEndofif.java
+++ JavaEndofif.java
@@ -5,11 +5,23 @@ public class Solution {
 
     public static void main(String[] args) {
       Scanner sc =new Scanner(System.in);
-      String s1= sc.nextLine();
-      String s2 =sc.nextLine();
-      String s3 =sc.nextLine();
-      System.out.println("1 "+s1);
-      System.out.println("2 "+s2);
-      System.out.println("3 "+s3);
+    //   String s1= sc.nextLine();
+    //   String s2 =sc.nextLine();
+    //   String s3 =sc.nextLine();
+    //   System.out.println("1 "+s1);
+    //   System.out.println("2 "+s2);
+    //   System.out.println("3 "+s3);
+    
+    
+        int numberline = 1;
+    String n = null;
+    while (sc.hasNextLine()) {
+        n = sc.nextLine();
+        System.out.println(numberline + " " + n);
+
+        numberline++;
+
+
+    
     }
-}
+}}
